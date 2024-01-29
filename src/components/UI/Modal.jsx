859168fab1d3fc649b84f0b60159f6ea46cb5bc0
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-29 08:47:05 +0800

    progress

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-export default function Modal({ children, open, className = '' }) {
+export default function Modal({ children, open, className = "" }) {
   const dialog = useRef();
   useEffect(() => {
     if (open) {
@@ -10,7 +10,9 @@ export default function Modal({ children, open, className = '' }) {
   }, [open]);
 
   return createPortal(
-    <dialog ref={dialog} className={`modal ${className}`}>{children}</dialog>,
+    <dialog ref={dialog} className={`modal ${className}`}>
+      {children}
+    </dialog>,
     document.getElementById("modal")
   );
 }
